dc6b95d9df2176fdb86112fadc3639a64fe367f3
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-11-23 09:55:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit", addToDo)
+
+  function addToDo(event) {
+    event.preventDefault();
+
+    const taskList = document.getElementById("tasks")
+    const newTask = document.createElement("li")
+    newTask.textContent = taskForm.getElementsByTagName("input")[0].value
+    taskList.append(newTask)
+  }
 });
